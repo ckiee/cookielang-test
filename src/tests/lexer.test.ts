a5b0d42eafa7848090428a8fcfc27ca3d1e94c26
ronthecookie <ronthecookie0101@gmail.com>
2019-11-30 13:07:56 +0200

    Refactor lexer tests

--- src/tests/lexer.test.ts
+++ src/tests/lexer.test.ts
@@ -1,37 +1,38 @@
-import Lexer from "../lexer";
+import Lexer, { Token } from "../lexer";
+import TokenType from "../tokenType";
 
 test("it lexes comment", () => {
-    const tokens = new Lexer("//hello").lex();
-    expect(tokens).toEqual([{ match: "//hello", type: "comment" }]);
+    const tokens = new Lexer("// hello").lex();
+    expect(tokens).toEqual([new Token(TokenType.Comment, "// hello")]);
 
     const tokens2 = new Lexer("/*hello\nworld*/").lex();
-    expect(tokens2).toEqual([{ match: "/*hello\nworld*/", type: "comment" }]);
+    expect(tokens2).toEqual([new Token(TokenType.Comment, "/*hello\nworld*/")]);
 });
 
 test("it lexes keywords", () => {
     const tokens = new Lexer("fn").lex();
-    expect(tokens).toEqual([{ match: "fn", type: "keywordFn" }]);
+    expect(tokens).toEqual([new Token(TokenType.KeywordFn, "fn")]);
 });
 
 test("it lexes identifier", () => {
     const tokens = new Lexer("world").lex();
-    expect(tokens).toEqual([{ match: "world", type: "identifier" }]);
+    expect(tokens).toEqual([new Token(TokenType.Identifier, "world")]);
 });
 
 test("it lexes int", () => {
     const tokens = new Lexer("1337").lex();
-    expect(tokens).toEqual([{ match: "1337", type: "int" }]);
+    expect(tokens).toEqual([new Token(TokenType.Int, "1337")]);
 });
 
 test("it lexes float", () => {
     const tokens = new Lexer("30.14").lex();
-    expect(tokens).toEqual([{ match: "30.14", type: "float" }]);
+    expect(tokens).toEqual([new Token(TokenType.Float, "30.14")]);
 });
 
 test("it lexes whitespace", () => {
     const tokens = new Lexer("            \n\n    ").lex();
     expect(tokens).toEqual([
-        { match: "            \n\n    ", type: "whitespace" }
+        new Token(TokenType.Whitespace, "            \n\n    ")
     ]);
 });
 

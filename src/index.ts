f5d47b6f5156eb94faa266b082fbf32a15044121
ronthecookie <me@ronthecookie.me>
2019-12-06 16:28:14 +0200

    Fix import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Lexer from "./lexing/lexer";
 import Parser from "./parsing";
-import TokenStream from "./tokenStream";
+import TokenStream from "./util/tokenStream";
 import Node, { NodeType } from "./parsing/node";
 import CCodeGenPass from "./passes/ccodegenpass";
 import Function from "./parsing/function";

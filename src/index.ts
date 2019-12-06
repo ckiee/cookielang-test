7eacb1b1f505dd5028a62997b1d9b21dd7ebf154
ronthecookie <me@ronthecookie.me>
2019-12-06 16:21:16 +0200

    Expose some classes

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Lexer from "./lexing/lexer";
+import Parser from "./parsing";
+import TokenStream from "./tokenStream";
+import Node, { NodeType } from "./parsing/node";
+import CCodeGenPass from "./passes/ccodegenpass";
+import Function from "./parsing/function";
+
+export { Lexer, Parser, TokenStream, Node, NodeType, CCodeGenPass, Function };

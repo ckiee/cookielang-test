1f2bbf0ea1bfa66e7f32d779f3066fe1f084409c
Ron B <me@ronthecookie.me>
2019-12-09 16:54:35 +0200

    Add expr codegen

--- src/ccodegen/cnode.ts
+++ src/ccodegen/cnode.ts
@@ -5,10 +5,11 @@ import Type from "../parsing/type";
 import Value, { ValueType } from "../parsing/value";
 import Int from "../parsing/values/int";
 import String from "../parsing/values/string";
-import VarAccess from "../parsing/values/varRef";
+import Expr, { ExprType } from "../parsing/values/expr";
+import VariableRef from "../parsing/values/varRef";
 
 export default abstract class CNode {
-    static proto(returnType: CType, args: Arg[], id: string) {
+    static proto(returnType: CType, args: Arg[], id: string): string {
         const builder = new CBuilder()
             .add(returnType)
             .add(id)
@@ -19,16 +20,16 @@ export default abstract class CNode {
         builder.add(CSymbol.ParenR);
         return builder.toString();
     }
-    static type(type: Type) {
+    static type(type: Type): string {
         return new CBuilder().add(type.value).toString();
     }
-    static arg(arg: Arg) {
+    static arg(arg: Arg): string {
         return new CBuilder()
             .add(arg[0])
             .add(this.type(arg[1]))
             .toString();
     }
-    static block(statements: string[]) {
+    static block(statements: string[]): string {
         const builder: CBuilder = new CBuilder().add(CSymbol.BraceL);
 
         for (const statement of statements) {
@@ -37,7 +38,7 @@ export default abstract class CNode {
 
         return builder.add(CSymbol.BraceR).toString();
     }
-    static string(value: String) {
+    static string(value: String): string {
         return new CBuilder()
             .add(CSymbol.DubQuote, false)
             .add(value.value, false)
@@ -45,35 +46,45 @@ export default abstract class CNode {
             .toString();
     }
 
-    static fn(proto: string, body: string) {
+    static fn(proto: string, body: string): string {
         return new CBuilder()
             .add(proto.toString())
             .add(body.toString())
             .toString();
     }
-    static int(node: Int) {
+    static int(node: Int): string {
         return node.value.toString();
     }
 
-    static value(value: Value) {
+    static value(value: Value): string {
         switch (value.valueType) {
             case ValueType.Int:
                 return this.int(<Int>value);
             case ValueType.String:
                 return this.string(<String>value);
-            // TODO: case ValueType.Expr: 
+            case ValueType.Expr:
+                return this.expr(<Expr>value);
             default:
                 throw new Error("unknown valuetype");
         }
     }
-    static varDeclare(vType: string, id: string, value: Value) {
+    static expr(value: Expr): string {
+        switch (value.exprType) {
+            case ExprType.VariableRef:
+                return (<VariableRef>value).id
+            // case FnCall
+            default:
+                throw new Error("unknown ExprType " + value.exprType)
+        }
+    }
+    static varDeclare(vType: string, id: string, value: Value): string {
         const builder = new CBuilder().add(vType).add(id);
 
         builder.add(CSymbol.Equal).add(this.value(value));
 
         return builder.add(CSymbol.SemiColon).toString();
     }
-    static fnCall(id: string, args: Value[]) {
+    static fnCall(id: string, args: Value[]): string {
         const builder = new CBuilder().add(id).add(CSymbol.ParenL);
         for (let i in args) {
             const arg = args[i];
@@ -83,7 +94,7 @@ export default abstract class CNode {
         builder.add(CSymbol.ParenR).add(CSymbol.SemiColon);
         return builder.toString();
     }
-    static import(file: String) {
+    static import(file: String): string {
         return new CBuilder()
             .add(CKeyword.PreprocessInclude)
             .add(this.string(file))
